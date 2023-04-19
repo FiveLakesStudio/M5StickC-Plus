f8f851142b25afea53c1589d6fc6e7227f0eabf7
Tod Cunningham <tod.cunningham@gmail.com>
2023-04-19 00:13:12 -0400

    Display MAC address

--- DistanceMonitorScanner/DistanceMonitorScanner.ino
+++ DistanceMonitorScanner/DistanceMonitorScanner.ino
@@ -1,15 +1,15 @@
-#include <string.h> 
+#include <string.h>
 #include <M5StickCPlus.h>
 #include "UtilLcd.h"
 #include "UtilBleScanner.h"
 
 const uint32_t BackgroundColor = BLACK;
 const uint32_t TextColor = GREEN;
-const uint8_t  TextSizeBase = 8;
-const uint8_t  TextSize = 3;
-const uint8_t  TextSizeBig = TextSize + 1;
-const uint8_t  ScreenRotation90Degrees = 1;
-const uint8_t  ScreenRotation270Degrees = 3;
+const uint8_t TextSizeBase = 8;
+const uint8_t TextSize = 3;
+const uint8_t TextSizeBig = TextSize + 1;
+const uint8_t ScreenRotation90Degrees = 1;
+const uint8_t ScreenRotation270Degrees = 3;
 const unsigned long SerialPortBaudRate = 115200;
 
 const uint32_t LoopDelayMs = 250;
@@ -19,7 +19,7 @@ UtilBleScanner bleScanner;
 void setup() {
   M5.begin();
 
-  M5.Lcd.setRotation(ScreenRotation90Degrees);  
+  M5.Lcd.setRotation(ScreenRotation90Degrees);
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.fillScreen(BackgroundColor);
   M5.Lcd.setTextColor(TextColor, BackgroundColor);
@@ -35,9 +35,23 @@ void loop() {
   M5.update();
 
   M5.Lcd.setCursor(0, TextSize * TextSizeBase * 0);
-  M5.Lcd.print("Scanning...");  clearToEndOfLine();
-  
+  M5.Lcd.print("Scanning..."); clearToEndOfLine();
+
   bleScanner.findDevices();
+
+  if (bleScanner.foundDeviceRivian != nullptr) {
+    M5.Lcd.setCursor(0, TextSize * TextSizeBase * 1);
+    M5.Lcd.print("Rivian: ");
+    M5.Lcd.print(bleScanner.foundDeviceRivian->getAddress().toString().c_str());
+    clearToEndOfLine();
+  }
+
+  if (bleScanner.foundDeviceTesla != nullptr) {
+    M5.Lcd.setCursor(0, TextSize * TextSizeBase * 2);
+    M5.Lcd.print("Tesla: ");
+    M5.Lcd.print(bleScanner.foundDeviceTesla->getAddress().toString().c_str());
+    clearToEndOfLine();
+  }
 
   delay(LoopDelayMs); // Wait for a second before sending the next message
 }
